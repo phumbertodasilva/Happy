fea8a40f1a8269d631fde0bd1094a6c880c7e5a0
Paulo Humberto <phumbertodasilva@gmail.com>
2020-10-12 17:05:32 -0300

    Adicionado configuração de Mapa e Env de exemplo

--- web/src/pages/OrphanagesMap.tsx
+++ web/src/pages/OrphanagesMap.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import {Link} from "react-router-dom";
-import { FiPlus } from "react-icons/fi";
-import { Map, TileLayer } from "react-leaflet";
+import {FiPlus} from "react-icons/fi";
+import {Map, TileLayer} from "react-leaflet";
 
 import 'leaflet/dist/leaflet.css';
 
@@ -27,11 +27,13 @@ function OrphanagesMap() {
       </aside>
 
       <Map
-        center={[-16.6686775,-49.2740054]}
+        center={[-16.6686775, -49.2740054]}
         zoom={15}
-        style={{ width: '100%', height: '100%' }}
+        style={{width: '100%', height: '100%'}}
       >
-        <TileLayer url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+        {/*<TileLayer url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png" />*/}
+        <TileLayer
+          url={`https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`}/>
       </Map>
 
       <Link to="" className="create-orphanage">
